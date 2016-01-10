12ab9e194ae45b7af658725911b7c50fa40d84fe
rk23 <reed.kinning@gmail.com>
2016-01-10 12:54:58 -0800

    better regex only because it allows ' necessary in french, but still allows . and quotes, etc

--- app/controllers/translate_controller.rb
+++ app/controllers/translate_controller.rb
@@ -2,22 +2,20 @@ class TranslateController < ApplicationController
   def index
 
     #Uses microsoft-translator gem
+    #Need to change these to ENV variables!
     translator = MicrosoftTranslator::Client.new('01928374', 'Rw8yP/mFE41m/Hp7pPJ4TORa5b4unbsPCIJva7jtKh0=')
 
     #Grab data from the form on the main page
     @form_data = params.require(:translate).permit(:text, :lang)
 
     @text = @form_data['text']
-    @counted = count_words @text
-
-    #Gets total word count
     @word_count = @text.split(' ').length
 
-    #Sorts by frequency in descending order and returns a hash
-    @sorted =  @counted.sort_by {|word, freq| freq}.reverse
+    #Counts frequency and stores in hash
+    @parsed_and_sorted = count_words @text
 
     # @translation = get_translation translator, @text, 'es', 'en'
-     @translation = get_translation translator, @sorted, 'es', 'en'
+     @translation = get_translation translator, @parsed_and_sorted, 'fr', 'en'
 
 
   end
@@ -25,15 +23,15 @@ class TranslateController < ApplicationController
   private
 
   def parse_text(string)
-    words = string.gsub!(/[^\p{Alnum}]/, ' ').split(' ')
+    words = string.gsub("(.*?)($|'|[^[:punct:]]+?)(.*?)", "\\2").split(' ')
     words
   end
 
   def count_words(string)
     words = parse_text(string)
     frequency = Hash.new(0)
-    words.each { |word| frequency[word.downcase] += 1 }
-    frequency
+    words.each { |word| frequency[word] += 1 }
+    frequency.sort_by {|word, freq| freq}.reverse
   end
 
   def get_translation(translator, hash, lang_from, lang_to)
@@ -44,7 +42,6 @@ class TranslateController < ApplicationController
     hash.each do |word|
         translation[word[0]] = {count: word[1],
                                 translation: translator.translate(word[0],lang_from,lang_to,"text/html")}
-
     end
 
     translation
