a364f612570cad4cef165c75d09017d1e860e86b
cynhw <cynhowong@gmail.com>
2016-01-12 15:25:16 -0800

    Added jQuery and CSS property to create scrollbar in Word List Side Bar

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -23,3 +23,12 @@ $(function(){
     })
 
 });
+
+
+// Adding jQuery Plugin into Word List Sidebar
+
+(function($){
+    $(window).load(function(){
+    $(".right-sidebar").mCustomScrollbar();
+        });
+    })(jQuery);
