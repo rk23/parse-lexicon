d0b8f866d124946e38fcf63be80e2b1b934729aa
cynhw <cynhowong@gmail.com>
2016-01-13 03:09:41 -0800

    stylesheet is currently a mess, will revisit tomorrow

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( noun_book.png )
+Rails.application.config.assets.precompile += %w( background.png )
+Rails.application.config.assets.precompile += %w( half.png )
