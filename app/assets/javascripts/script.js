436a79a726b00996e63e6b44f7e720e2c5cf86e8
rk23 <reed.kinning@gmail.com>
2016-01-13 10:52:54 -0800

    script file comments

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -24,23 +24,34 @@ $(function(){
             dataType: 'html',
             data: {word: div.context.children[0].value}
         }).success(function(data){
+            //Frequency of word in current text
             var frequency = parseInt(div.attr('value'));
 
+            //Word count in current text
             var wordCount = parseFloat($('#percentage').attr('name'));
+
+            //User unknown word count in text, hence the subtraction when word added
             var userWordCount = parseFloat($('#percentage').attr('value')) - frequency;
             var known_percent = getPercentage(userWordCount, wordCount);
 
+            //Unique word count in current text
             var uniqueWordCount = parseFloat($('#unique-ratio').attr('name'));
+
+            //User unknown unique word count, hence the subtraction when word added
             var uniqueUserWordCount = parseFloat($('#unique-ratio').attr('value')) - 1;
             var unique_percent = getPercentage(uniqueUserWordCount, uniqueWordCount);
 
+            //Update percentages as well as stored values used in this script
             $('#percentage').attr('value', userWordCount);
             $('#percentage').html(known_percent);
             $('#unique-ratio').attr('value', uniqueUserWordCount);
             $('#unique-ratio').html(unique_percent);
 
+            //Remove the word count button div then remove the
+            //highlight class on associated words
             div.remove();
             $('.parse-' + data.replace("'", "_")).removeClass()
+
         }).error(function(err){
             console.log(err);
         })
