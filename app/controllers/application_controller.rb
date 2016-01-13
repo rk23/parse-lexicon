6b85ebe3c1d74c8e70e176bc7e67e1614787a14d
djpo <derekpoppe@gmail.com>
2016-01-13 00:31:59 -0800

    Fixed navbar so it hides only on 'welcome' view. Changed 'welcome' and 'home' views from partials to regular views; moved their conditional logic to main_controller.rb.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,8 @@ class ApplicationController < ActionController::Base
     # hash associating language codes and language names (and flags?)
   end
 
-  def disable_nav
-    @disable_nav = true
+  def disable_navbar
+    @disable_navbar = true
   end
 
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,15 +1,21 @@
 class MainController < ApplicationController
 	before_action :is_authenticated?
-	before_filter :disable_nav, only: [:index]
   
   def index
+  	# If user is not logged in, disable navbar and display welcome view instead of home view
+  	if current_user
+  		render "home"
+  	else
+  		disable_navbar
+  		render "welcome"
+  	end
+
   end
 
   def about
-
   end
 
   def recommended
-
   end
+
 end
