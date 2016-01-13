49f6db5d87d901d646a3d2bc95f42deba71f0b06
djpo <derekpoppe@gmail.com>
2016-01-12 23:25:22 -0800

    Added language list to application_controller.rb; dropdown menus access language list.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Allows use of language list and conversion between language name and symbol
+  before_action :access_languages
+
   def is_authenticated?
     unless current_user
       flash[:danger] = "Credentials invalid"
@@ -14,8 +17,21 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by_id(session[:user_id])
   end
 
-  def lang
-    # hash associating language codes and language names (and flags?)
+  def access_languages
+    # Hash associating language codes and language names (and flags?)
+      # Working, but incomplete list
+    @language_hash = {
+      :en => 'English',
+      :bg => 'Bulgarian',
+      :ca => 'Catalan'
+    }
+
+    # Array of languages in the format [['French', 'fr'], ...] -- for use in dropdown menus
+    @language_array = []
+    @language_hash.each do |symbol, name|
+      @language_array.push([name, symbol.to_s])
+    end
+
   end
 
 end
