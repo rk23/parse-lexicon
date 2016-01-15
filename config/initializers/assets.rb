fd99b8e90f0d3c406caf927b2a49593b62dac1a8
cynhw <cynhowong@gmail.com>
2016-01-14 22:41:08 -0800

    styling the welcome page before user logs in

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( background.png )
 Rails.application.config.assets.precompile += %w( half.png )
+Rails.application.config.assets.precompile += %w( parse_bg.jpg )
